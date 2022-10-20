74ba4ada84bfcf6a5106dc98479d7755ebed32e8
unknown <muryllo.henrique@hotmail.com>
2022-10-20 10:38:53 -0300

    aprendendo hooks

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <PrimeiroComponente />
+  <LifeCycle />
 </template>
 
 <script>
 import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import LifeCycle from "./components/LifeCycle.vue"
 
 export default {
   name: "App",
   components: {
     PrimeiroComponente,
+    LifeCycle,
   },
 };
 </script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LifeCycle",
+  data() {
+    return {
+      nome: "Muryllo",
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.nome = "Muryllinho";
+    }, 1000);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = "Muryllao";
+    }, 2000);
+  },
+};
+</script>
