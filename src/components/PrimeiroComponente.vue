17f21ba8f80e3679899b0a5c2a00cb1ad0603236
unknown <muryllo.henrique@hotmail.com>
2022-10-20 10:22:09 -0300

    adicionando funcionalidades

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Olá Vue!</h1>
+  <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+</template>
+
+<script>
+export default {
+  name: "PrimeiroComponente",
+  data() {
+    return {
+      nome: "Muryllo",
+      profissao: "Dev Front-End",
+    };
+  },
+};
+</script>
