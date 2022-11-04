50734abfd8fccb62863a243f951ab8eb6f781801
unknown <muryllo.henrique@hotmail.com>
2022-11-04 16:49:32 -0300

    aprendendo diretivas

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,9 +1,18 @@
 <template>
-  <form action="">
-    <InputText />
-    <InputText />
-    <Submit />
-  </form>
+  <div>
+    <h2>Meu formulário:</h2>
+    <form action="">
+      <div>
+        <InputText />
+      </div>
+      <div>
+        <InputText />
+      </div>
+      <div>
+        <Submit />
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 import InputText from "./form/InputText.vue";
--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,14 +1,24 @@
 <template>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
         <li>TS</li>
         <li>React</li>
         <li>Vue</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+    <p v-if="3 > 2">Testando</p>
 </template>
 <script>
 export default {
     name: 'info',
+    data() {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: true,
+            email: 'muryllo@email.com'
+        }
+    }
 }
 </script>
